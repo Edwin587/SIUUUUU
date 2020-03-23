b27950474d22a8886cc81c76b6f5d28e1ef909a1
Mario Isaac <ax.minck@gmail.com>
2020-03-22 20:03:26 -0600

    Desde C7

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace SIUUUUU
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.txtMesagge.Text = "Enviando Misiles a la Luna...";
+        }
     }
 }
